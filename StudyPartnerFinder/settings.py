6cd6684485664a29d22631979aed8a1d324636c1
Nathan Oliphant <nathan@oliphantparts.org>
2018-11-20 20:10:33 -0800

    Updated user profile to just include user profile, since user studygroups can be found under user studygroups.  Switched to using JavaScript Block in templates.

--- StudyPartnerFinder/settings.py
+++ StudyPartnerFinder/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-# STATIC_ROOT = '/home/no2/StudyPartnerFinder/StudyBuddy/static/'
+# STATIC_ROOT = '/home/no2/StudyPartnerFinder/StudyPartnerFinder/static/'
 STATIC_ROOT = '/Users/nathan/Documents/workspace/StudyPartnerFinder/static/'
 STATIC_URL = '/static/'
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
